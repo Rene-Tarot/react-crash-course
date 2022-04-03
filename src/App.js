74d2bb5ec5095c6be8534bbc7448fd60e5c0206c
Rene-Tarot <rene.tarot@galileo.edu>
2022-04-02 21:05:11 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from './components/AddTask';
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctor',
+        day: 'Tomorow',
+        reminder: true,
+    }
+  ])
+
+  //Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task))
+  }
+
+  return (
+    <div className="container">
+      <Header />
+      <AddTask />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}
+       onToggle={toggleReminder}/> : 'No tasks to show.'}
+    </div>
+  );
+}
+
+export default App;
